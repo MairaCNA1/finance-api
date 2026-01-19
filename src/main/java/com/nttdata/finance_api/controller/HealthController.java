5936d6f93b976aa959fc7122479a6d74b8aa065b
Maíra <maira.assis@estudante.iftm.edu.br>
2026-01-19 20:59:45 -0300

    chore: projeto base com Spring Boot, Docker e PostgreSQL

---
+++ src/main/java/com/nttdata/finance_api/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.nttdata.finance_api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "API FINANCE OK";
+    }
+}
